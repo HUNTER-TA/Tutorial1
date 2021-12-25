610f702e113c146966bec48baa7ac17d46769513
HUNTER <hunter@ad.hunterta.net>
2021-12-25 15:43:03 +0900

    Initial commit

---
+++ src/main/java/net/hunterta/mc/plugins/tutorial/tutorial1/Tutorial1.java
@@ -0,0 +1,17 @@
+package net.hunterta.mc.plugins.tutorial.tutorial1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tutorial1 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
